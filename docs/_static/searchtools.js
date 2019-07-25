29de66b29eadbc4e64d41503ccc79044e4fc0278
Yoshiki Shibukawa <yoshiki@shibu.jp>
2019-07-25 21:44:14 +0900

    build

--- docs/_static/searchtools.js
+++ docs/_static/searchtools.js
@@ -75,16 +75,6 @@ var Search = {
       }
   },
 
-  loadIndex : function(url) {
-    $.ajax({type: "GET", url: url, data: null,
-            dataType: "script", cache: true,
-            complete: function(jqxhr, textstatus) {
-              if (textstatus != "success") {
-                document.getElementById("searchindexloader").src = url;
-              }
-            }});
-  },
-
   setIndex : function(index) {
     var q;
     this._index = index;
@@ -319,13 +309,12 @@ var Search = {
     for (var prefix in objects) {
       for (var name in objects[prefix]) {
         var fullname = (prefix ? prefix + '.' : '') + name;
-        var fullnameLower = fullname.toLowerCase()
-        if (fullnameLower.indexOf(object) > -1) {
+        if (fullname.toLowerCase().indexOf(object) > -1) {
           var score = 0;
-          var parts = fullnameLower.split('.');
+          var parts = fullname.split('.');
           // check for different match types: exact matches of full name or
           // "last name" (i.e. last dotted part)
-          if (fullnameLower == object || parts[parts.length - 1] == object) {
+          if (fullname == object || parts[parts.length - 1] == object) {
             score += Scorer.objNameMatch;
           // matches in last name
           } else if (parts[parts.length - 1].indexOf(object) > -1) {
